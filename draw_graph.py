e0f7bcd3433793ea54f64382e4a93232447d36ff
Jerome Dockes <jerome@dockes.org>
2026-05-15 17:25:14 +0200

    gh-pages

--- draw_graph.py
+++
@@ -1,8 +0,0 @@
-import electricity_load_forecasting as elf
-
-pred = elf.make_data_op()
-g = pred.skb.draw_graph()
-with open("graph.svg", "wb") as f:
-    f.write(g.svg)
-with open("graph.png", "wb") as f:
-    f.write(g.png)
--- report.py
+++
@@ -1,6 +0,0 @@
-import electricity_load_forecasting as elf
-
-env = elf.get_env()
-pred = elf.make_data_op()
-pred.skb.eval(env)
-pred.skb.full_report(env)
